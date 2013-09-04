5a02e22a9dc9c87e1ded9b04e395cb41e3cc2d67
Austin Keeley <austin.keeley@gmail.com>
2013-09-03 20:15:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AustinkeeleyCom::Application.config.secret_token = '7ab574e280360fd0da70b61f7be5166261aa7a913cb42f1ac9679d2caee41bc05c25106e977b1d52187a5f64e28e7734a4996c670da584e7a38be5fd81e21db2'
