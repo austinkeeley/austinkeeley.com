f2471af818eed82216ec330f0402565805778ce9
Austin Keeley <austin.keeley@gmail.com>
2013-09-08 21:18:45 -0400

    Switching to ruby 1.8 syntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AustinkeeleyCom::Application.config.session_store :cookie_store, key: '_austinkeeley.com_session'
+AustinkeeleyCom::Application.config.session_store :cookie_store, :key => '_austinkeeley.com_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
